69b13eadbab445ccbdb272b2f2dff764ac34f532
lovenorman <90197758+lovenorman@users.noreply.github.com>
2022-09-20 20:03:34 +0200

    MainViewModel

--- Device.Climate/MVVM/ViewModels/MainViewModel.cs
+++ Device.Climate/MVVM/ViewModels/MainViewModel.cs
@@ -8,6 +8,28 @@ namespace Device.Climate.MVVM.ViewModels
 {
     internal class MainViewModel : ObservableObject    
     {
+        public MainViewModel()
+        {
+            KitchenView = new KitchenViewModel();
+            CurrentView = KitchenView;
+
+            KitchenViewCommand = new RelayCommand(x => { CurrentView = KitchenView; });
+        }
         
+        private object _currentView;
+        
+        public RelayCommand KitchenViewCommand { get; set; }
+
+        public KitchenViewModel KitchenView { get; set; }
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
