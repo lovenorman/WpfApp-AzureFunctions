0d305c68c082124e9967eca5bfaae80abb6a4b84
lovenorman <90197758+lovenorman@users.noreply.github.com>
2022-09-27 18:13:23 +0200

    Changed TileComponent Name, changed back. Some bug

--- Device.Climate/MVVM/Models/DeviceItem.cs
+++ Device.Climate/MVVM/Models/DeviceItem.cs
@@ -8,9 +8,14 @@ namespace Device.Climate.MVVM.Models
 {
     internal class DeviceItem
     {
-        public string DeviceId { get; set; }
-        public string DeviceName { get; set; }
-        public string DeviceType { get; set; }
-        public string ConnectionState { get; set; }
+        public string DeviceId { get; set; } = "";
+        public string DeviceName { get; set; } = "";
+        public string DeviceType { get; set; } = "";
+
+
+        public string IconActive { get; set; } = "";
+        public string IconInActive { get; set; } = "";
+        public string StateActive { get; set; } = "";
+        public string StateInActive { get; set; } = "";
     }
 }
