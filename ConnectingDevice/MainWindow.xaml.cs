7ba9b63ee21d0bfa04b339b673b7b0def1998d70
lovenorman <90197758+lovenorman@users.noreply.github.com>
2022-10-06 13:47:15 +0200

    SendMessageAsync

--- ConnectingDevice/MainWindow.xaml.cs
+++ ConnectingDevice/MainWindow.xaml.cs
@@ -19,6 +19,9 @@ using System.Data;
 using System.Net.Http;
 using System.Net.Http.Json;
 using ConnectingDevice.Models;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+using Microsoft.Azure.Devices.Shared;
 
 namespace ConnectingDevice
 {
@@ -26,8 +29,8 @@ namespace ConnectingDevice
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
-        private readonly string _connectUrl = "http://localhost:7225/api/devices/connect";//URL som fås från APIet (vilket jag inte har än)
+    {                                          //https://sysytemutvecklingfunctionapp.azurewebsites.net
+        private readonly string _connectUrl = "http://localhost:7225/api/devices/connect";
         private readonly string _connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\love_\\source\\repos\\Inlämningsuppgift\\ConnectingDevice\\Data\\device_DB.mdf;Integrated Security=True;Connect Timeout=30";
 
         private DeviceClient _deviceClient;
@@ -42,10 +45,11 @@ namespace ConnectingDevice
         public MainWindow()
         {
             InitializeComponent();
-            Setup();
+            SetupAsync().ConfigureAwait(false);
+            SendMessageAsync().ConfigureAwait(false);
         }
 
-        public async Task Setup()
+        public async Task SetupAsync()
         {
             using IDbConnection conn = new SqlConnection(_connectionString);
             var deviceId = await conn.QueryFirstOrDefaultAsync<string>("SELECT DeviceId FROM DeviceInfo");
@@ -68,14 +72,50 @@ namespace ConnectingDevice
             
         }
 
-        private void Loop()
+        private async Task SendMessageAsync()
         {
+            while(true)
+            {
+                if(_connected)
+                {
+                    if(_lightState != _lightPreviusState)
+                    {
+                        _lightPreviusState = _lightState;
 
+                        //d2c
+                        var json = JsonConvert.SerializeObject(new {lightState = _lightState});
+                        var message = new Message(Encoding.UTF8.GetBytes(json));
+                            message.Properties.Add("deviceName", deviceInfo.DeviceName);
+                            message.Properties.Add("deviceType", deviceInfo.DeviceType);
+                            message.Properties.Add("location", deviceInfo.Location);
+                            message.Properties.Add("owner", deviceInfo.Owner);
+
+                        await _deviceClient.SendEventAsync(message);
+                        tbStateMessage.Text = $"Message sent at {DateTime.Now}.";
+
+                        //device twin (reported)
+                        var twinCollection = new TwinCollection();
+                        twinCollection["lightState"] = _lightState;
+                        await _deviceClient.UpdateReportedPropertiesAsync(twinCollection);
+                    }
+                }
+                await Task.Delay(_interval);
+            }
         }
 
         private void btnOnOff_Click(object sender, RoutedEventArgs e)
         {
-            
+            _lightState = !_lightState;
+
+            if (_lightState)
+            {
+                btnOnOff.Content = "Turn Off";
+            }
+            else
+            {
+                btnOnOff.Content = "Turn On";
+            }
+
         }
     }
 }
